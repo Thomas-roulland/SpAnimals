7da3ba131b080e2e07e4cd581305ce82c0d54be6
Thomas Roulland <troulland@sio.local>
2021-10-15 11:00:32 +0200

    up

--- src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
+++ src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
@@ -1,8 +1,13 @@
 package edu.supavenir.spanimals.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
 
+	@GetMapping("contact")
+	private String ContactAction() {
+		return "contact";
+	}
 }

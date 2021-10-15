d9dae66a8fc10234dafe707f4e70779e2be4e47b
Thomas Roulland <troulland@sio.local>
2021-10-15 11:59:24 +0200

    up

--- src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
+++ src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
@@ -1,13 +1,29 @@
 package edu.supavenir.spanimals.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import edu.supavenir.spanimals.models.Espece;
+import edu.supavenir.spanimals.repositories.AnimalRepository;
+import edu.supavenir.spanimals.repositories.EspeceRepository;
+
 
 @Controller
 public class SpaController {
 
-	@GetMapping("contact")
-	private String ContactAction() {
-		return "contact";
+	@Autowired
+	private EspeceRepository repo;
+	
+	@GetMapping("recherche")
+	private  String ContactAction(Model model) {
+		List<Espece> especes = repo.findAll();
+		model.addAttribute("especes", especes);
+		return "recherche";
 	}
+	
+	
 }

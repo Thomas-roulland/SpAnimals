b02490058c144d33e9b64a78a38043a9d22d8ed5
Mathieu-Dumont <73880080+Mathieu-Dumont@users.noreply.github.com>
2021-10-15 11:59:59 +0200

    'up'

--- src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
+++ src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
@@ -1,13 +1,21 @@
 package edu.supavenir.spanimals.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import edu.supavenir.spanimals.models.Animal;
+import edu.supavenir.spanimals.repositories.AnimalRepository;
+
 @Controller
 public class SpaController {
-
+	
 	@GetMapping("contact")
 	private String ContactAction() {
 		return "contact";
 	}
+	
+	
 }
--- src/main/java/edu/supavenir/spanimals/repositories/AnimalRepository.java
+++ src/main/java/edu/supavenir/spanimals/repositories/AnimalRepository.java
@@ -1,5 +1,7 @@
 package edu.supavenir.spanimals.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import edu.supavenir.spanimals.models.Animal;
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Integer> {
 
+	List<Animal> findBySos(boolean SOS);
+		
 }

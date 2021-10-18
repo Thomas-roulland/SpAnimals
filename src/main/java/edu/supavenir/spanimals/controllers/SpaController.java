e6598dbe0dac642a6297864319b103df100291f3
Thomas Roulland <troulland@sio.local>
2021-10-18 12:51:18 +0200

    up

--- src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
+++ src/main/java/edu/supavenir/spanimals/controllers/SpaController.java
@@ -1,7 +1,6 @@
 package edu.supavenir.spanimals.controllers;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -16,33 +15,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.supavenir.spanimals.models.Espece;
-<<<<<<< HEAD
 import edu.supavenir.spanimals.models.Refuge;
-import edu.supavenir.spanimals.repositories.AnimalRepository;
 import edu.supavenir.spanimals.repositories.EspeceRepository;
 import edu.supavenir.spanimals.repositories.RefugeRepository;
-=======
-import edu.supavenir.spanimals.repositories.EspeceRepository;
->>>>>>> c17ca9ffacabf79a70ac297fe0a8547f232f109b
 
 @Controller
 public class SpaController {
 
     @Autowired
     private EspeceRepository repo;
-<<<<<<< HEAD
+
     @Autowired
     private RefugeRepository repoR;
-    @Autowired
-    private AnimalRepository repoA;
-=======
->>>>>>> c17ca9ffacabf79a70ac297fe0a8547f232f109b
 
     @GetMapping("recherche")
     private String ContactAction(Model model) {
 	List<Espece> especes = repo.findAll();
 	model.addAttribute("especes", especes);
-<<<<<<< HEAD
+
 	List<Refuge> refuges = repoR.findAll();
 	model.addAttribute("refuges", refuges);
 	return "recherche";
@@ -54,24 +44,17 @@ public class SpaController {
 	Map<String, String[]> map = request.getParameterMap();
 	String s = "";
 	String test = "";
+	String test1 = "";
 	Connection connection = null;
 
 	for (Map.Entry<String, String[]> entry : map.entrySet()) {
 	    s += ("Key = " + entry.getKey() + ", Value = " + entry.getValue()[0]) + "<br>";
 	    if (entry.getValue() != null) {
-		test = entry.getKey() + "= :" + entry.getKey() + "";
+		test += entry.getKey() + "= :" + entry.getKey();
 	    }
 
 	}
-	PreparedStatement requete = connection.prepareStatement("SELECT * FROM Animal WHERE ");
-	return s;
-
+	test1 = "SELECT * FROM Animal WHERE " + test + ";";
+	return test1;
     }
 }
-=======
-	return "recherche";
-
-    }
-
-}
->>>>>>> c17ca9ffacabf79a70ac297fe0a8547f232f109b

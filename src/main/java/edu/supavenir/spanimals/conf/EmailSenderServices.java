fc89a459d2bcf7a49f63b9e02c008f156ba8baad
ROULLAND <73873954+Thomas-roulland@users.noreply.github.com>
2022-05-09 11:31:40 +0200

    envoie mail + attachement

--- src/main/java/edu/supavenir/spanimals/conf/EmailSenderServices.java
+++ src/main/java/edu/supavenir/spanimals/conf/EmailSenderServices.java
@@ -1,9 +1,16 @@
 package edu.supavenir.spanimals.conf;
 
 
+import java.io.File;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +28,25 @@ public class EmailSenderServices {
 		message.setSubject(subject);
 		
 		mailSender.send(message);
-		System.out.println("Mail Send with succes");
+		System.out.println("Mail send with succes");
+	}
+	
+	public void sendEmailWithAttachment(String toEmail,String body, String subject, String attachment) throws MessagingException {
+		
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
+		
+		MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+		
+		mimeMessageHelper.setFrom("spanimals.project@gmail.com");
+		mimeMessageHelper.setTo(toEmail);
+		mimeMessageHelper.setText(body);
+		mimeMessageHelper.setSubject(subject);
+		FileSystemResource fileSystem = new FileSystemResource(new File(attachment));
+		
+		mimeMessageHelper.addAttachment(fileSystem.getFilename(), 
+				fileSystem);
+		
+		mailSender.send(mimeMessage);
+		System.out.println("Mail with attachment send with succes !");
 	}
 }
